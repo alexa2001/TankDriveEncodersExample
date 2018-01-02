a54c99dbcddc5e29908a6f5ceff5725c81ce1e7a
alexa2001 <amlagunas04@gmail.com>
2018-01-01 21:24:19 -0600

    Update ExampleSubsystem.java

--- src/org/usfirst/frc/team4645/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team4645/robot/subsystems/ExampleSubsystem.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.Encoder;
 public class ExampleSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
+	
+	Encoder sampleEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
