2ca33400ed8a1c2125e654b36e4092ccc13ed93a
alexa2001 <amlagunas04@gmail.com>
2018-01-03 00:30:32 -0600

    Update RobotMap.java

--- src/org/usfirst/frc/team4645/robot/RobotMap.java
+++ src/org/usfirst/frc/team4645/robot/RobotMap.java
@@ -26,4 +26,14 @@ public class RobotMap {
 	public static int joystick1 = 1;
 	public static int joystick2= 2;
 	
+	/* Left encoder Digital I/O ports.
+	 */
+	public static final int LEFT_ENCODER_1 = 2;
+	public static final int LEFT_ENCODER_2 = 3;
+	    /**
+	     * Right encoder Digital I/O ports.
+	     */
+	public static final int RIGHT_ENCODER_1 = 4;
+	public static final int RIGHT_ENCODER_2 = 5;	
+
 }
